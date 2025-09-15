7852e879ed3f57006ffd45ca8aab230d8b769f27
Herbert97 <falcon9718@hotmail.com>
2025-09-14 23:11:14 -0600

    cambios lectura de datos

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 import pandas as pd
 import plotly.express as px
 import streamlit as st
+import os
 
-car_data = pd.read_csv(r"C:\Users\falco\OneDrive\Documentos\SPRINT7\vehicles_us.csv") # leer los datos
+# obtener la ruta del archivo en la misma carpeta que app.py
+file_path = os.path.join(os.path.dirname(__file__), "vehicles_us.csv")
+
+car_data = pd.read_csv(file_path)  # leer los datos
+###
+#car_data = pd.read_csv(r"C:\Users\falco\OneDrive\Documentos\SPRINT7\vehicles_us.csv") # leer los datos
 hist_button = st.button('Construir histograma') # crear un botón
-     
+   
 if hist_button: # al hacer clic en el botón
     # escribir un mensaje
     st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
